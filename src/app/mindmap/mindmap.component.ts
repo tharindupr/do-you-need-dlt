28e74c29dbdf412d3c96e9ed18bdd4b3df64bd55
tharindupr <tharindu.prf@gmail.com>
2019-06-26 13:53:13 +0100

    mind map component

---
+++ src/app/mindmap/mindmap.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from "d3";
+
+@Component({
+  selector: 'app-mindmap',
+  templateUrl: './mindmap.component.html',
+  styleUrls: ['./mindmap.component.css']
+})
+export class MindmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    
+  }
+
+}
